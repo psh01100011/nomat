1319300b8930b97a6b5c3141b58c27a86423c12f
jason.park <eofpsh@gmail.com>
2026-08-10 16:19:00 +0900

    feat: 이미지 업로드 검증 및 보상 처리 강화

--- src/main/java/com/dogdog/nomat/domain/asset/controller/UploadController.java
+++ src/main/java/com/dogdog/nomat/domain/asset/controller/UploadController.java
@@ -4,11 +4,13 @@ import com.dogdog.nomat.domain.asset.dto.UploadImageResponse;
 import com.dogdog.nomat.domain.asset.service.AssetService;
 import com.dogdog.nomat.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,6 +22,7 @@ public class UploadController {
     private final AssetService assetService;
 
     @PostMapping("/images")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<UploadImageResponse> uploadImage(
             @AuthenticationPrincipal Jwt jwt,
             @RequestParam MultipartFile file
